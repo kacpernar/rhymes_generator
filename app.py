e58a9188c804f500d3f066c8b0dedd34fe53c2af
Numpad <morek1@op.pl>
2023-01-20 21:14:03 +0100

    Desktop app update

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
 # Using flask to make an api
 # import necessary libraries and functions
-from flask import Flask, jsonify, request, abort, Response
+from flask import Flask, jsonify, request, abort, Response, render_template
 import json
 from flask_cors import CORS
 from score import prepare_model
+from flaskwebgui import FlaskUI
+import mimetypes
 
 import enchant
 from nltk.corpus import cmudict
 
 from rhyme import rhyme
 
+
 # creating a Flask app
 app = Flask(__name__)
 CORS(app)
+
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('text/css', '.css')
+
 dict = enchant.Dict("en_US")
 
 # Create an instance of the CMU pronunciation dictionary
 d = cmudict.dict()
 model,tokenizer = prepare_model()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    if (request.method == 'GET'):
-
-        data = "hello world"
-        return jsonify({'data': data})
-
+    return render_template('index.html')
 
 @app.route('/rhymes/<word>/<treshold>/<amount>', methods=['GET'])
 def disp(word,treshold, amount):
@@ -35,5 +38,5 @@ def disp(word,treshold, amount):
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    #app.run(debug=True)
+    FlaskUI(app=app, width=1000, height=700, server="flask", port="5000").run() 
