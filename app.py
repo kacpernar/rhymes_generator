a35d9c4aba6324f2786ec833637c775e72ee637c
Numpad <morek1@op.pl>
2023-02-05 20:30:33 +0100

    Backend omits scores

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ from nltk.corpus import cmudict
 from rhyme import rhyme, polish_rhyme
 from rhymes import get_dictionary, rhymes_generator
 
-
+def f(wordg):
+    if(float(wordg["score"]) >= 0):
+        wordg["score"] = ""
+        return wordg
 
 # creating a Flask app
 app = Flask(__name__)
@@ -37,16 +40,19 @@ def home():
 @app.route('/rhymes/<word>/<treshold>/<amount>', methods=['GET'])
 def disp(word,treshold, amount):
     rhymes = rhyme(word, d, dict,model,tokenizer, treshold, amount)
-    json_rhymes = jsonify(rhymes)
+    my_dictionary = list(map(f, rhymes))
+    json_rhymes = jsonify(my_dictionary)
     return json_rhymes
 
 @app.route('/pl/<word>/<level>/<amount>', methods=['GET'])
 def dispp(word, level, amount):
     rhymes = polish_rhyme(dicts['pl'], word, level, amount)
-    json_rhymes = jsonify(rhymes)
+    my_dictionary = list(map(f, rhymes))
+    json_rhymes = jsonify(my_dictionary)
     return json_rhymes
 
 
 if __name__ == '__main__':
     # app.run(debug=True)
     FlaskUI(app=app, width=1000, height=700, server="flask", port="5000").run() 
+
